0d37b13d5da196526559a06d75d7b5925c56a598
assinaturasnetflix <jonaspedroqt@gmail.com>
2025-05-12 18:21:27 +0200

    Update Page.tsx

--- src/app/Page.tsx
+++ src/app/Page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import Image from 'next/image';
 import { useRef } from 'react';
 import LogoSection from '../components/LogoSection';
 import ResultsSection from '../components/ResultsSection';
